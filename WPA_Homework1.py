364537d4e7bebdef0cb768cd9e30a50d5caf412a
TerezaPalkova <palkova.tereza@gmail.com>
2020-07-18 14:51:14 +0200

    Upravena uloha

---
+++ WPA_Homework1.py
@@ -0,0 +1,34 @@
+#pyramida 1
+zoz=[1,2,3,4,5,6,7,8,9,10]
+
+for n in zoz :
+    print("*"* n)
+    n+=1
+
+
+#pyramida 2
+l=9
+for h in range(10):
+    print("*"*(l+1))
+    l-=1
+
+#pyramida 2 (bez extra premennej)
+for h in range (10):
+    print("*"*((h+10)-2*h))
+
+
+#prestupny rok
+year= int(input("Choose a year:" ))
+
+if ((year % 4 == 0) and (year % 100 != 0)) or ((year % 4 == 0) and (year % 400 == 0)) :
+    print (f"rok {year} je prestupny")
+else:
+    print (f"rok{year} je neprestupny")
+
+# prestupny rok range
+years= range(2000,2002)
+for year in years:
+    if ((year % 4 == 0) and (year % 100 != 0)) or ((year % 4 == 0) and (year % 400 == 0)) :
+       print(f"rok {year} je prestupny")
+    else:
+        print(f"rok {year} je neprestupny")
