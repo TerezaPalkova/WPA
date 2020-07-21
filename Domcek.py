7a43cdf4601755230fd41acd2d4cc53a98fbd8e1
TerezaPalkova <palkova.tereza@gmail.com>
2020-07-21 14:56:18 +0200

    Uloha WPA 3

--- Domcek.py
+++ Domcek.py
@@ -15,9 +15,9 @@ def domcek (d):
     d.forward((math.sqrt(((200**2)*2))/2))
     d.left(90)
     d.forward(math.sqrt((200**2)*2))
-t=turtle.Turtle()
+#t=turtle.Turtle()
 
-domcek(t)
+#domcek(t)
 time.sleep(2)
 
 #domaca uloha
@@ -26,17 +26,22 @@ def pyramida (zakladna,smer,centrovanie):
     for h in range(zakladna):
         if centrovanie== "center":
            if smer == "obratena":
-              print(str("*"*(zakladna-h)).center(2*zakladna)) #neviem kde robim chybu, ze mi to centruje tak zvlastne 
+               if h%2 != 0:
+                    row = "*" * (zakladna - h)
+                    print (row.center(zakladna))
+
            else:
-              print(str("*" * (h + 1)).center(2*zakladna))
-              h+=1
+                if h % 2 == 0:
+                    print(str("*" * (h + 1)).center(zakladna))
+
         else:
            if smer == "obratena":
-              print("*"*(zakladna-h))
+                 print("*"*(zakladna-h))
            else:
-              print("*" * (h + 1))
-              h += 1
+                 print("*" * (h + 1))
+                 h += 1
 
-pyramida(10, "obratena","center")
+pyramida(10, "obraten","center")
 
 
+#
