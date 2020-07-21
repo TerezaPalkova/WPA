969ba9fde758d07beb09755c6e96c4af8772eaef
TerezaPalkova <palkova.tereza@gmail.com>
2020-07-21 14:03:57 +0200

    Upravena uloha

---
+++ Domcek.py
@@ -0,0 +1,40 @@
+import time
+import turtle
+import math
+
+
+def domcek (d):
+    for x in range(4):
+        d.forward(200)
+        d.left(90)
+    d.left(45)
+    d.forward(math.sqrt(((200**2)*2)))
+    d.left(90)
+    d.forward((math.sqrt(((200**2)*2))/2))
+    d.left(90)
+    d.forward((math.sqrt(((200**2)*2))/2))
+    d.left(90)
+    d.forward(math.sqrt((200**2)*2))
+t=turtle.Turtle()
+
+domcek(t)
+time.sleep(2)
+
+#domaca uloha
+
+def pyramida (zakladna,smer,centrovanie):
+    for h in range(zakladna):
+        if centrovanie== "center":
+           if smer == "obratena":
+              print(str("*"*(zakladna-h)).center(2*zakladna))
+           else:
+              print(str("*" * (h + 1)).center(2*zakladna))
+              h+=1
+        else:
+           if smer == "obratena":
+              print("*"*(zakladna-h))
+           else:
+              print("*" * (h + 1))
+              h += 1
+
+pyramida(10, "obratena","center")
