67b6b00c2c6fa7d3d6140aeaba469841eee849ef
TerezaPalkova <palkova.tereza@gmail.com>
2020-07-22 09:59:37 +0200

    Uloha WPA 3

--- Domcek.py
+++ Domcek.py
@@ -23,24 +23,46 @@ time.sleep(2)
 #domaca uloha
 
 def pyramida (zakladna,smer,centrovanie):
+    """
+        Funkcia pyramida.
+
+        Parametre:
+        1.argument (int): velkost zakladne
+        2.argument (str): smer pyramidy
+            "normalna"
+            "obratena"
+        3.argument (str): centrovanie pyramidy
+            "center"
+            "vlavo"
+
+        """
     for h in range(zakladna):
         if centrovanie== "center":
            if smer == "obratena":
                if h%2 != 0:
                     print (str("*" * (zakladna - h)).center(zakladna))
 
-           else:
+           elif smer == "normalna":
                 if h % 2 == 0:
                     print(str("*" * (h + 1)).center(zakladna))
+           else:
+               print (f"Zadali ste zly udaj na druhom mieste. Prosim upravte slovo {smer}")
+               break
 
-        else:
+        elif centrovanie == "vlavo":
            if smer == "obratena":
                  print("*"*(zakladna-h))
            else:
                  print("*" * (h + 1))
                  h += 1
+        else:
+            print(f"Zadali ste zly udaj na tretom mieste. Prosim upravte slovo {centrovanie}")
+            break
 
-pyramida(10, "obratena","center")
+
+pyramida(10, "normalna","center")
+
+print (pyramida.__doc__)
 
 
 #
