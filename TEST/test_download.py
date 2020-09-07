a9361ff315755093bb4e576fcd0de05238a68214
TerezaPalkova <palkova.tereza@gmail.com>
2020-09-07 19:57:47 +0200

    TEST pic-downloader prva verzia

--- TEST/test_download.py
+++ TEST/test_download.py
@@ -1,15 +1,7 @@
 from unittest import TestCase
 from TEST.pic_downloaderr import download
 
-
 class TestDownload(TestCase):
-    def download_type(self):
-        znaky = count_words(data, pismena=True)
-        self.assertIsInstance(znaky, dict)
-
-
-pass
-
-
-class TestDownload(TestCase):
-    pass
+    def test_download(self):
+        pass
+        self.fail()
